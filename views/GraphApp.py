564317b8c26d3bf6a5545d92fef6f4c2e46113a2
Josiah Glyshaw <josiah@glyshaw.com>
2022-10-19 14:17:18 -0500

    important refactor

--- views/GraphApp.py
+++ views/GraphApp.py
@@ -3,8 +3,6 @@
 from PyQt6.QtWidgets import *
 from controllers.Graph3D import Graph3D
 from controllers.MLEngine import MLEngine
-import matlab.engine
-import random
 
 class GraphApp(QWidget):
     def __init__(self): 
@@ -20,11 +18,11 @@ class GraphApp(QWidget):
 
     def initUI(self):
         #buttons
-        button = QPushButton("Graph")
-        button.clicked.connect(self.graphNeuron)
+        button = QPushButton("Nodes")
+        button.clicked.connect(self.graphNodes)
 
-        graph2 = QPushButton("Graph2")
-        graph2.clicked.connect(self.graphNeuron2)
+        graph2 = QPushButton("Edges")
+        graph2.clicked.connect(self.graphEdges)
 
         clear = QPushButton("Clear")
         clear.clicked.connect(lambda : self.graph.clearGraph())
@@ -38,43 +36,35 @@ class GraphApp(QWidget):
         #add widgets
         self.window.addWidget(self.graph,0,0)
         self.window.addWidget(button,1,0)
-        self.window.addWidget(clear,2,0)
+        self.window.addWidget(clear,4,0)
         self.window.addWidget(move,3,0)
-        self.window.addWidget(graph2,4,0)
+        self.window.addWidget(graph2,2,0)
 
 
-    def graphNeuron(self):
+    def graphNodes(self):
         nodes = self.MLEngine.nodes(self.neuron)
         x = []
         y = []
         z = []
-
         for id in nodes:
             x1,y1,z1 = nodes[id]
             x.append(x1)
             y.append(y1)
             z.append(z1)
-
         self.graph.scatterPlot(x,y,z)
 
        
-    def graphNeuron2(self):
+    def graphEdges(self):
         nodes = self.MLEngine.nodes(self.neuron)
         edges = self.MLEngine.edges(self.neuron, nodes)
-      
-        x = []
-        y = []
-        z = []
-        for edge in edges:
-            
-            x.append(edge[0][0])
-            x.append(edge[1][0])
-            y.append(edge[0][1])
-            y.append(edge[1][1])
-            z.append(edge[0][2])
-            z.append(edge[1][2])
-        self.graph.linePlot(x,y,z)
 
+        for edge in edges:
+            x = [edge[0][0],edge[1][0]]
+            y = [edge[0][1],edge[1][1]]
+            z = [edge[0][2],edge[1][2]]
+            self.graph.linePlot(x,y,z)
+        self.graph.draw()
+        self.graph.fig.canvas.setFocus()
        
 
 
